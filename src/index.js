8475967edd24a7744b469e94c874383ed3d4fdb4
levy225 <levy.zire@gmail.com>
2021-01-16 02:32:28 +0100

    Revert "try to instrospect"
    This reverts commit 6c73d63d6ed4b6c74ea10c100a4afcc3ca3aa19b.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 const { GraphQLServer } = require('graphql-yoga');
-const {Prisma} = require('prisma-binding');
-const resolvers = require('./resolvers');
-require('dotenv').config()
 
+const typeDefs = `
+    type Query {
+        hello: String!
+    }
+`;
 
+const resolvers = {
+    Query: {
+        hello: (_, args, context, info) => {
+            return "Hello World";
+        }
+    }
+};
 
-const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
-  resolvers,
-  context: req => ({
-    req,
-    prisma: new Prisma({
-      typeDefs: './src/generated/prisma.graphql',
-      endpoint:
-        process.env.URL_DB_PRISMA,
-    }),
-  }),
-});
-
+const server = new GraphQLServer({typeDefs, resolvers});
 server.start(() => console.log('Server is running on localhost:4000'));
